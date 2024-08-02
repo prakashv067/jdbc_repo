c168dbe702b13df8ba47a901a32d8508cdd274a6
91908 <91908@Prakash>
2024-08-02 13:19:18 +0530

    insert

---
+++ Jdbc/src/show_prepared/Main.java
@@ -0,0 +1,24 @@
+package show_prepared;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	
+	Class.forName("com.mysql.jdbc.Driver");
+	
+	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+	
+	PreparedStatement preparedStatement = connection.prepareStatement("insert into emp values(?,?)");
+	
+	preparedStatement.setInt(1, 4);
+	preparedStatement.setString(2, "sweaty");
+	
+	preparedStatement.executeUpdate();
+	
+	connection.close();
+}
+}
